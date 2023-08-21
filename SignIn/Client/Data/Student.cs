00df06da6318a27ddad46927d5c2f651f6a0d4d8
firstgenstudios <123118486+devfrontender@users.noreply.github.com>
2023-08-21 17:25:50 -0400

    initial commit

---
+++ SignIn/Client/Data/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SignIn.Client.Data
+{
+    public class Student
+    {
+        [Required]
+        [StringLength(10, ErrorMessage = "Username is too long.")]
+        public string? Username { get; set; } = null!;
+
+        [Required]
+        [StringLength(18, ErrorMessage = "Password is too long.")]
+        public string? Password { get; set; } = null!;
+    }
+}
---
+++ SignIn/Client/Pages/Index.razor.cs
@@ -0,0 +1,16 @@
+﻿using SignIn.Client.Data;
+
+namespace SignIn.Client.Pages
+{
+    public partial class Index
+    {
+        public Student? Model { get; set; } = null!;
+
+        protected override void OnInitialized() => Model ??= new();
+
+        private void Submit()
+        {
+            Logger.LogInformation("Model.Username = {Username}", Model?.Username);
+        }
+    }
+}
